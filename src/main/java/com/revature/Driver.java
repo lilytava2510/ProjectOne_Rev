08deacf4db952480037748f39d9216d7206b131f
lilytava2510 <42839204+lilytava2510@users.noreply.github.com>
2022-05-09 11:39:55 -0400

    monday05/09

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,13 @@
 package com.revature;
 
+import com.revature.utils.ConnectionSingleton;
+
+import java.sql.Connection;
+
 public class Driver {
 
-
+public static void main(String[] args ) {
+    Connection c = ConnectionSingleton.getConnectionSingleton().getConnection();
+}
 
 }
